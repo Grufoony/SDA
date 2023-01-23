d1818db7991e759381780296de90adc796fe7c99
Grufoony <gregorio_2000@hotmail.it>
2023-01-23 14:42:20 +0100

    sort of what it should be

--- script.R
+++ script.R
@@ -3,35 +3,26 @@ df <- read.csv("./data/population.csv")
 library(ggplot2)
 
 head(df)
-unique(df$Country)
-head(filter)
-
-filter <- df[df$Country == "China",]
-ggplot(data = filter, aes(x = Year, y = MedianAge)) +
-    geom_point() + geom_smooth(method = "lm")
-ggplot(data = filter, aes(x = Year, y = FertilityRate)) +
-    geom_point() + geom_smooth(method = "lm")
-
-head(filter)
+unique(df$Continent)
 
 ###############################################################################
 # POPULATION GROWTH ANALISYS ==== LINEAR REGRESSION
 ###############################################################################
 
 # USA ANALYSIS
-usa <- df[df$Country == "United States",]
+usa <- df[df$Country == "United States", ]
 
 model <- lm(Population ~ Year, data = usa)
 summary(model)
 
 ggplot(data = usa, aes(x = Year, y = Population)) +
     geom_point() + geom_smooth(method = "lm") +
-    labs(title = "USA Population", x = "Year", y = "Population")
+    labs(title = "U.S.A. Population", x = "Year", y = "Population")
 
 # PAKISTAN ANALYSIS
 # Pakistan has an exponential growth -> log scale to fit a linear model
 
-pak <- df[df$Country == "Pakistan",]
+pak <- df[df$Country == "Pakistan", ]
 pak[, "log_Population"] <- log(pak[, "Population"] + 1)
 
 model <- lm(log_Population ~ Year, data = pak)
@@ -39,45 +30,61 @@ summary(model)
 
 ggplot(data = pak, aes(x = Year, y = Population)) +
     geom_point() + geom_smooth(method = "lm") +
-    labs(title = "USA Population", x = "Year", y = "Population")
+    labs(title = "Pakistan population", x = "Year", y = "Population")
 ggplot(data = pak, aes(x = Year, y = log_Population)) +
-    geom_point() + geom_smooth(method = "lm") + 
-    labs(title = "USA Population", x = "Year", y = "Population")
+    geom_point() + geom_smooth(method = "lm") +
+    labs(title = "Pakistan Population", x = "Year", y = "Population logarithm")
 
 ###############################################################################
-# ==== HYPOTHESIS TESTING
+# POPULATION GROWTH BETWEEN CONTINENTS==== HYPOTHESIS TESTING
 ###############################################################################
 
-pop1 <- df[df$Country == "United States", "Population"]
-pop2 <- df[df$Country == "Italy", "Population"]
+unique(df$Continent)
 
-ggplot( data = df, aes(x=Population) ) + geom_histogram( binwidth = 100000, color="black", fill="skyblue")
+oceania <- log(df[df$Continent == "Oceania", "Population"])
+aisa <- log(df[df$Continent == "Asia", "Population"])
 
-qqnorm(pop1, main = "Normal qq-plot (Population 1)", ylab = "Empirical Quantiles", xlab = "Theoretical Quantiles")
-qqnorm(pop2, main = "Normal qq-plot (Population 2)", ylab = "Empirical Quantiles", xlab = "Theoretical Quantiles")
-qqline(pop1, col="red", lty=4, lwd=2)
-qqline(pop2, col="red", lty=4, lwd=2)
+ggplot() +
+    geom_histogram(aes(x = oceania), fill = "red", alpha = 0.5, bins = 20) +
+    geom_histogram(aes(x = asia), fill = "blue", alpha = 0.5, bins = 20)
 
-t.test(pop1, pop2)
+qqnorm(oceania, main = "Normal qq-plot (Oceania)",
+    ylab = "Empirical Quantiles", xlab = "Theoretical Quantiles")
+qqline(oceania, col = "red", lty = 4, lwd = 2)
+qqnorm(asia, main = "Normal qq-plot (Population 2)",
+    ylab = "Empirical Quantiles", xlab = "Theoretical Quantiles")
+qqline(asia, col = "red", lty = 4, lwd = 2)
 
-############################################################################################################################################################################
-# FERTILITY RATE ANALYSIS ==== MULTILINEAR REGRESSION
-############################################################################################################################################################################
+t.test(oceania, asia)
 
-filter <- df[df$Country == "Albania",]
+###############################################################################
+# YEARLY CHANGE ==== MULTILINEAR REGRESSION
+###############################################################################
+
+head(df)
+filter <- df[df$Country == "France", ]
+
+result <- lm(Yearly.Change ~Population + Migrants.net.
+    + FertilityRate + MedianAge + UrbanPopulation, data = filter)
+summary(result)
+
+
+###############################################################################
+###############################################################################
+#other things to try
+
 corYearAge <- cor(filter[, "Year"], filter[, "MedianAge"])
 corFertilityYear <- cor(filter[, "FertilityRate"], filter[, "Year"])
 corAgeFertility <- cor(filter[, "MedianAge"], filter[, "FertilityRate"])
-print(c(corAgeYear, corFertilityYear, corAgeFertility))
+print(c(corYearAge, corFertilityYear, corAgeFertility))
 
-result <- lm(FertilityRate ~ Year + MedianAge, data = filter)
-summary(result)
 
-ggplot(data = filter, aes(x = Year, y = MedianAge)) + geom_point() + geom_smooth(method = "lm")
+# plots are not that good
+
+ggplot(data = filter, aes(x = Migrants.net., y = Yearly.Change)) + geom_point() + geom_smooth(method = "lm")
+ggplot(data = filter, aes(x = Migrants.net., y = FertilityRate)) + geom_point() + geom_smooth(method = "lm")
+ggplot(data = filter, aes(x = Migrants.net., y = MedianAge)) + geom_point() + geom_smooth(method = "lm")
+ggplot(data = filter, aes(x = Migrants.net., y = UrbanPopulation)) + geom_point() + geom_smooth(method = "lm")
 
 ggplot(data = filter, aes(x = MedianAge, y = exp_FertilityRate)) + geom_point() + geom_smooth(method = "lm")
 
-
-medianAge <- filter[, "MedianAge"]
-fertilityRate <- filter[, "FertilityRate"]
-t.test(medianAge, fertilityRate)
